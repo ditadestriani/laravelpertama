f93b0a28aca2ce3824adf3dc524f62483e260ab4
indahali <indahalisahida@gmail.com>
2020-10-25 19:58:14 +0700

    commit5

--- resources/views/friends/coba.blade.php
+++ resources/views/friends/coba.blade.php
@@ -1,6 +1,7 @@
 @extends('layouts.app')
 
 @section('title', 'cobaaaa')
+
 @section('content')
-urutan ke - {{ $ke }}
+    urutan ke - {{ $ke }}
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,7 @@
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
 
-    <title>@yield ('title')</title>
+    <title>@yield('title')</title>
   </head>
   <body>
   <div class="container">
@@ -27,10 +27,6 @@
         <a href="/friends/create" class="card-link btn-primary">Tambah Teman</a>
 
     @yield('content')
-
-
-
-
 
     </div>
     <!-- Optional JavaScript; choose one of the two! -->
