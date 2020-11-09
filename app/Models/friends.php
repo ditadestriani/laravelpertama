34e5f23d15bbb8a51d3c673dcebc23c5a2e08b82
indahali <indahalisahida@gmail.com>
2020-11-09 17:40:09 +0700

    commit7

--- app/Models/friends.php
+++ app/Models/friends.php
@@ -5,8 +5,14 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class friends extends Model
+class Friends extends Model
 {
     use HasFactory;
+
     protected $guarded = ['mana'];
+
+    public function groups()
+    {
+        return $this->belongsTo('App\Models\Groups');
+    }
 }
